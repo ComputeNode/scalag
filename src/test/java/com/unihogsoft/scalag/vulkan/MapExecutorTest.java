43bb96654a8e303aaff8fa9c05fb598561c17503
MarconZet <marcin.zlakowski@gmail.com>
2020-04-28 16:47:27 +0200

    Implemented test for multiple buffers map transformation

--- src/test/java/com/unihogsoft/scalag/vulkan/MapExecutorTest.java
+++ src/test/java/com/unihogsoft/scalag/vulkan/MapExecutorTest.java
@@ -48,7 +48,7 @@ class MapExecutorTest {
         ByteBuffer shaderCode;
         try {
             ClassLoader classLoader = getClass().getClassLoader();
-            File file = new File(Objects.requireNonNull(classLoader.getResource("comp.spv")).getFile());
+            File file = new File(Objects.requireNonNull(classLoader.getResource("copy.spv")).getFile());
             FileInputStream fis = new FileInputStream(file);
             FileChannel fc = fis.getChannel();
             shaderCode = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
@@ -90,5 +90,63 @@ class MapExecutorTest {
         memFree(output[0]);
 
         assertArrayEquals(in, out);
+    }
+
+    private static final int n = 2;
+
+    @Test
+    void copyRandomDataBetweenFourBuffers(){
+        ByteBuffer shaderCode;
+        try {
+            ClassLoader classLoader = getClass().getClassLoader();
+            File file = new File(Objects.requireNonNull(classLoader.getResource("two_copy.spv")).getFile());
+            FileInputStream fis = new FileInputStream(file);
+            FileChannel fc = fis.getChannel();
+            shaderCode = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        List<BindingInfo> bindingInfos = new ArrayList<>(4);
+        bindingInfos.add(new BindingInfo(0, 4, BINDING_TYPE_INPUT));
+        bindingInfos.add(new BindingInfo(1, 4, BINDING_TYPE_INPUT));
+        bindingInfos.add(new BindingInfo(2, 4, BINDING_TYPE_OUTPUT));
+        bindingInfos.add(new BindingInfo(3, 4, BINDING_TYPE_OUTPUT));
+
+        Vector3ic workgroupDimensions = new Vector3i(128, 1, 1);
+
+        Shader shader = new Shader(shaderCode, workgroupDimensions, bindingInfos, "main", context.getDevice());
+        MapPipeline pipeline = new MapPipeline(shader, context);
+
+        MapExecutor executor = new MapExecutor(bufferLength, pipeline, context);
+
+        Random rand = new Random(System.currentTimeMillis());
+        byte[][] randData = new byte[n][bufferSize];
+        ByteBuffer[] input = new ByteBuffer[n];
+        for(int i = 0; i<n; i++){
+            rand.nextBytes(randData[i]);
+            input[i] = MemoryUtil.memAlloc(bufferSize);
+            input[i].put(randData[i]).flip();
+        }
+
+        ByteBuffer[] output = executor.execute(input);
+
+        executor.destroy();
+        pipeline.destroy();
+        shader.destroy();
+
+        byte[][] out = new byte[n][bufferSize];
+        for(int i = 0; i<n; i++){
+            output[i].get(out[i]);
+        }
+
+        for(int i = 0; i<n; i++){
+            memFree(input[i]);
+            memFree(output[i]);
+        }
+
+        for(int i = 0; i<n; i++){
+            assertArrayEquals(randData[i], out[i]);
+        }
     }
 }
--- src/test/resources/compile.ps1
+++ src/test/resources/compile.ps1
@@ -1,1 +1,2 @@
-"$Env:VULKAN_SDK\Bin\glslangValidator.exe -V shader.comp" | Invoke-Expression
+"$Env:VULKAN_SDK\Bin\glslangValidator.exe -V copy.comp -o copy.spv" | Invoke-Expression
+"$Env:VULKAN_SDK\Bin\glslangValidator.exe -V two_copy.comp -o two_copy.spv" | Invoke-Expression
