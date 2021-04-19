12c937233e7c5a822787acf0e04abbc8a440f49f
MarconZet <marcin.zlakowski@gmail.com>
2021-04-19 22:24:35 +0200

    Added shader for initialising data

--- src/main/java/com/unihogsoft/scalag/vulkan/executor/SortByKeyExecutor.java
+++ src/main/java/com/unihogsoft/scalag/vulkan/executor/SortByKeyExecutor.java
@@ -31,6 +31,7 @@ public class SortByKeyExecutor extends AbstractExecutor {
     private final Shader keyShader;
 
     private final ComputePipeline keyPipeline;
+    private final ComputePipeline preparePipeline;
     private final ComputePipeline sortPipeline;
     private final ComputePipeline copyPipeline;
 
@@ -38,6 +39,7 @@ public class SortByKeyExecutor extends AbstractExecutor {
         super(dataLength, createBufferActions(), context);
         this.keyPipeline = keyPipeline;
         this.keyShader = keyPipeline.getComputeShader();
+        this.preparePipeline = createPreparePipeline(context);
         this.copyPipeline = createCopyPipeline(context);
         this.sortPipeline = createSortPipeline(context);
         setup();
@@ -77,8 +79,8 @@ public class SortByKeyExecutor extends AbstractExecutor {
                 createUpdatedDescriptorSet(sortPipeline.getDescriptorSetLayouts()[0], buffers.subList(1, 3)),
                 createUpdatedDescriptorSet(sortPipeline.getDescriptorSetLayouts()[1], buffers.subList(4, 6)),
                 createUpdatedDescriptorSet(sortPipeline.getDescriptorSetLayouts()[1], Arrays.asList(buffers.get(5), buffers.get(4))),
-                createUpdatedDescriptorSet(copyPipeline.getDescriptorSetLayouts()[0], Arrays.asList(buffers.get(0), buffers.get(4)))
-
+                createUpdatedDescriptorSet(copyPipeline.getDescriptorSetLayouts()[0], Arrays.asList(buffers.get(0), buffers.get(4))),
+                createUpdatedDescriptorSet(preparePipeline.getDescriptorSetLayouts()[0], Arrays.asList(buffers.get(2), buffers.get(4)))
         );
         this.descriptorSets.addAll(descriptorSets);
     }
@@ -92,6 +94,15 @@ public class SortByKeyExecutor extends AbstractExecutor {
             vkCmdBindDescriptorSets(commandBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, keyPipeline.getPipelineLayout(), 0, pDescriptorSets, null);
 
             Vector3ic workgroup = keyShader.getWorkgroupDimensions();
+            vkCmdDispatch(commandBuffer, dataLength / workgroup.x(), 1 / workgroup.y(), 1 / workgroup.z());
+
+
+            vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, preparePipeline.get());
+
+            pDescriptorSets = stack.longs(descriptorSets.get(5).get());
+            vkCmdBindDescriptorSets(commandBuffer, VK_PIPELINE_BIND_POINT_COMPUTE, preparePipeline.getPipelineLayout(), 0, pDescriptorSets, null);
+
+            workgroup = preparePipeline.getComputeShader().getWorkgroupDimensions();
             vkCmdDispatch(commandBuffer, dataLength / workgroup.x(), 1 / workgroup.y(), 1 / workgroup.z());
 
 
@@ -147,6 +158,21 @@ public class SortByKeyExecutor extends AbstractExecutor {
                         new LayoutInfo(0, 1, 4),
                         new LayoutInfo(1, 0, 4),
                         new LayoutInfo(2, 0, 4)
+                ),
+                "main",
+                context.getDevice()
+        );
+
+        return new ComputePipeline(shader, context);
+    }
+
+    private static ComputePipeline createPreparePipeline(VulkanContext context) {
+        Shader shader = new Shader(
+                Shader.loadShader("prepare.spv"),
+                new Vector3i(1024, 1, 1),
+                Arrays.asList(
+                        new LayoutInfo(0, 0, 4),
+                        new LayoutInfo(0, 1, 4)
                 ),
                 "main",
                 context.getDevice()
